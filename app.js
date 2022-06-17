5151e1084724f0e49ad5980345a7b00943f8a283
Aaron Goshine <greenlig@gmail.com>
2022-06-17 13:24:53 +0100

    feat: read url from request

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const http = require("http")
 
 http.createServer(function(req, res){
-    let referer = req.headers.referer || false
-    console.log(req.headers)
     res.writeHeader(200, {"Content-Type":"text/plain"})
-    res.write("Hello ")
-    //res.end()
-    if (req.headers.referer) {
-        console.log(referer)
-        var name = referer.split("/")[3]
-        console.log(name)
 
-        //console.log(req.headers)
-        res.writeHeader(200, {"Content-Type":"text/plain"})
-        res.write("Hello " + name)
-        //console.log("this worked")
-        res.end()
-    }
-}).listen(3000)
+    res.write("Hello " +  req.url)
+    console.log("this worked")
+  res.end()
+}).listen(3000, function (i) {
+  console.log('running')
+})
 
